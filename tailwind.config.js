133b16c9baf2cdd321bd6f3f57ab27d5310426f7
bucky0112 <bucky0112@gmail.com>
2023-02-19 14:45:00 +0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-btn": "#333333",
+        "secondary-btn": "#a6a6a6",
+        "accent-btn": "#ffc107",
+      }
+    },
   },
   plugins: [],
 }
