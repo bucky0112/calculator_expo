91186b3789ff425d8da55fc87e4707c3b1c68aeb
bucky0112 <bucky0112@gmail.com>
2023-02-23 01:37:32 +0800

    Update logic for sign toggle and calculator function

--- src/util/calculator.ts
+++ src/util/calculator.ts
@@ -1,91 +1,83 @@
 export type State = {
-  currentValue: string;
-  operator: string | null;
-  previousValue: string | null;
+  currentValue: string
+  operator: string | null
+  previousValue: string | null
 }
 
 export const initialState: State = {
   currentValue: '0',
   operator: null,
-  previousValue: null,
-};
+  previousValue: null
+}
 
 export const handleNumber = (value: string, state: State): State => {
-  if (state.currentValue === '0') {
-    return { currentValue: `${value}`, operator: state.operator, previousValue: state.previousValue };
-  }
-
   return {
-    currentValue: `${state.currentValue}${value}`,
-    operator: state.operator,
-    previousValue: state.previousValue,
-  };
-};
+    ...state,
+    currentValue:
+      state.currentValue === '0' ? `${value}` : `${state.currentValue}${value}`
+  }
+}
 
 const handleEqual = (state: State): State => {
-  const { currentValue, previousValue, operator } = state;
+  const { currentValue, previousValue = '0', operator } = state
 
-  const current = parseFloat(currentValue);
-  const previous = parseFloat(previousValue || '0');
-  const resetState = { operator: null, previousValue: null };
+  const current = parseFloat(currentValue)
+  const previous = parseFloat(previousValue as string)
+  const resetState = { operator: null, previousValue: null }
 
   switch (operator) {
     case '+':
       return {
-        currentValue: `${previous + current}`,
         ...resetState,
-      };
+        currentValue: `${previous + current}`
+      }
     case '-':
       return {
-        currentValue: `${previous - current}`,
         ...resetState,
-      };
+        currentValue: `${previous - current}`
+      }
     case '*':
       return {
-        currentValue: `${previous * current}`,
         ...resetState,
-      };
+        currentValue: `${previous * current}`
+      }
     case '/':
       return {
-        currentValue: `${previous / current}`,
         ...resetState,
-      };
-
+        currentValue: `${previous / current}`
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-// calculator function
 const calculator = (type: string, value: string, state: State): State => {
   switch (type) {
     case 'number':
-      return handleNumber(value, state);
+      return handleNumber(value, state)
     case 'clear':
-      return initialState;
-    case 'posneg':
+      return initialState
+    case 'toggleSign':
       return {
-        currentValue: `${parseFloat(state.currentValue) * -1}`,
-        operator: state.operator,
-        previousValue: state.previousValue,
-      };
+        ...state,
+        currentValue: `${parseFloat(state.currentValue) * -1}`
+      }
     case 'percentage':
       return {
-        currentValue: `${parseFloat(state.currentValue) * 0.01}`,
-        operator: state.operator,
-        previousValue: state.previousValue,
-      };
+        ...state,
+        currentValue: `${parseFloat(state.currentValue) * 0.01}`
+      }
     case 'operator':
       return {
         operator: value,
         previousValue: state.currentValue,
-        currentValue: '0',
-      };
+        currentValue: '0'
+      }
     case 'equal':
-      return handleEqual(state);
+      return handleEqual(state)
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default calculator;
+export default calculator
