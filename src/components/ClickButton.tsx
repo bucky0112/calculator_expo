e61e07bb20a29b1f050dea0b723223dac6f3f4af
bucky0112 <bucky0112@gmail.com>
2023-02-25 10:00:56 +0800

    Remove unused imports and styles from ClickButton component

--- src/components/ClickButton.tsx
+++ src/components/ClickButton.tsx
@@ -1,10 +1,8 @@
 import {
-  Dimensions,
   Text,
   TouchableOpacity,
   TextProps,
   TouchableOpacityProps,
-  StyleSheet
 } from 'react-native'
 import classNames from 'classnames'
 
@@ -54,37 +52,3 @@ const ClickButton = ({
 }
 
 export { ClickButton }
-
-const screen = Dimensions.get('window')
-const buttonWidth = screen.width / 4
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#333333",
-    flex: 1,
-    height: Math.floor(buttonWidth - 10),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: Math.floor(buttonWidth),
-    margin: 5,
-  },
-  text: {
-    color: "#fff",
-    fontSize: 24,
-  },
-  textSecondary: {
-    color: "#060606",
-  },
-  buttonDouble: {
-    width: screen.width / 2 - 10,
-    flex: 0,
-    alignItems: "flex-start",
-    paddingLeft: 40,
-  },
-  buttonSecondary: {
-    backgroundColor: "#a6a6a6",
-  },
-  buttonAccent: {
-    backgroundColor: "#ffc107",
-  },
-});
